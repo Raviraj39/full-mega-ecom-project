60baf02a162cabdc8646c4070c00fab1c88103e1
Raviraj39 <rravirajbujawade@gmail.com>
2023-01-30 17:19:40 +0530

    get product detail route done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,6 +16,22 @@ exports.getAllProduct = async (req, res) => {
   res.status(200).json({ success: true, products });
 };
 
+//Get product Info
+
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "product not found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
 //update product --admin
 
 exports.updateProduct = async (req, res, next) => {
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,12 +4,16 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
+  getProductDetails,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProduct);
-router.route("/products/new").get(createProduct);
-router.route("/products/:id").get(updateProduct).delete(deleteProduct);
-
+router.route("/products/new").post(createProduct);
+router
+  .route("/products/:id")
+  .put(updateProduct)
+  .delete(deleteProduct)
+  .get(getProductDetails);
 module.exports = router;
