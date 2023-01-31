72bde4c2a92d586d3eedba8ea7e9adef666b4af9
Raviraj39 <rravirajbujawade@gmail.com>
2023-02-01 01:17:09 +0530

    add more packages and getting ready an userSchema

---
+++ backend/controllers/useController.js
@@ -0,0 +1,3 @@
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsynErrors = require("../middleware/catchAsyncErrors");
+const User = require("../models/userModel");
---
+++ backend/models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    maxLength: [30, "Name cannot exceed 30 character"],
+    minLength: [4, "Name should have more than 4 character"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [8, "Password should have more than 8 character"],
+    select: false,
+  },
+  avtar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
